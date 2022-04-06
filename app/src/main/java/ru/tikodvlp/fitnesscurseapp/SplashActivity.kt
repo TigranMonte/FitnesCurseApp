6a94d8e1adee011ad2f9ce32e29d3417b6aca374
Tigran <tigranma@gmail.com>
2022-04-06 17:29:45 +0300

    FitnessCurseApp - list_item added, Splash fixed

--- app/src/main/java/ru/tikodvlp/fitnesscurseapp/SplashActivity.kt
+++ app/src/main/java/ru/tikodvlp/fitnesscurseapp/SplashActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
@@ -12,6 +13,7 @@ class SplashActivity : AppCompatActivity() {
     private lateinit var timer: CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_activity)
         timer = object : CountDownTimer(2000, 1000) {
