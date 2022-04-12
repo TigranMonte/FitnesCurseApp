f50866585d1d9b9584e817ec0b8d0be31012764d
Tigran <tigranma@gmail.com>
2022-04-12 21:19:26 +0300

    FitnessCurseApp - DaysFragment update

--- app/src/main/java/ru/tikodvlp/fitnesscurseapp/fragments/DaysFragment.kt
+++ app/src/main/java/ru/tikodvlp/fitnesscurseapp/fragments/DaysFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ru.tikodvlp.fitnesscurseapp.R
+import ru.tikodvlp.fitnesscurseapp.adapters.DayModel
 import ru.tikodvlp.fitnesscurseapp.databinding.FragmentDaysBinding
 
 class DaysFragment : Fragment() {
@@ -18,6 +19,14 @@ class DaysFragment : Fragment() {
     ): View {
         binding = FragmentDaysBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun fillDaysArray(): ArrayList<DayModel> {
+        val tempArray = ArrayList<DayModel>() // временный экземпляр класса для послед. заполнения
+        resources.getStringArray(R.array.day_exercise).forEach {
+            tempArray.add(DayModel(it, false))
+        }
+        return tempArray
     }
 
     companion object {
